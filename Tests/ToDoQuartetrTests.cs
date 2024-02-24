81e4f49f7bacb637cee4e8afd25564c1fcc68ab7
glabojko <glabojko@gmail.com>
2024-02-24 12:30:43 +0100

    FEAT: unit test to check if accesing an index in the list out of range is possible

--- Tests/ToDoQuartetrTests.cs
+++ Tests/ToDoQuartetrTests.cs
@@ -49,5 +49,33 @@ namespace Eisenhower_Matrix.Tests
 
             Assert.Throws<ArgumentException>(() => toDoQuarter.AddItem(id, title, deadline, isDone));
         }
+
+        [Test]
+        public void RemoveItem_ValidIndex_ShouldRemoveItem()
+        {
+            var toDoQuarter = new ToDoQuarter();
+            toDoQuarter.AddItem(1, "Task 1", DateTime.UtcNow.AddDays(1), false);
+            toDoQuarter.AddItem(2, "Task 2", DateTime.UtcNow.AddDays(2), false);
+
+            toDoQuarter.RemoveItem(1);
+
+            var items = toDoQuarter.GetItems();
+            Assert.AreEqual(1, items.Count, "Item count is incorrect");
+
+            var remainingItem = items[0];
+            Assert.AreEqual(1, remainingItem.Id, "Remaining item Id is incorrect");
+            Assert.AreEqual("Task 1", remainingItem.Title, "Remaining item Title is incorrect");
+            Assert.AreEqual(DateTime.UtcNow.AddDays(1), remainingItem.Deadline, "Remaining item Deadline is incorrect");
+            Assert.AreEqual(false, remainingItem.IsDone, "Remaining item IsDone status is incorrect");
+        }
+
+        [Test]
+        public void RemoveItem_IndexOutOfRange_ShouldThrowArgumentOutOfRangeException()
+        {
+            var toDoQuarter = new ToDoQuarter();
+            toDoQuarter.AddItem(1, "Task 1", DateTime.UtcNow.AddDays(1), false);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => toDoQuarter.RemoveItem(1));
+        }
     }
 }
